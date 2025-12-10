1fe4fc56be1559af77e02996b4c3330288ee7a32
SophieSchwager <sophie@restorationfund.org>
2025-12-10 12:05:50 -0800

    Some outlining

--- scripts/Sophie Script.R
+++ scripts/Sophie Script.R
@@ -1,7 +1,32 @@
+##Port of Seattle Report##
+#install packages#
 
 
-Sophie-script
 
-huh
+
+#####Temperature of all selected sites 2024-2025 (SURFACE)#####
+
+
+#####Temperature of all selected sites  2024-2025 (BOTTOM)#####
+
+
+
+#####Dissolved Oxygen of all selected sites 2025 (SURFACE)#####
+
+
+#####Dissolved Oxygen of all selected sited 2025 (BOTTOM)#####
+
+
+
+
+#####Kelp densities for all species deep vs. shallow years 2024 and 2025 stacked#####
+##(how do you stack densities/error bars)##
+
+
+
+
+#####Grouped kelp density for all selected sites 2024 and 2025#####
+
+
 
 
