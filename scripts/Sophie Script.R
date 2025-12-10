f26379c9c579913dbccf5fdd204843811d448351
SophieSchwager <sophie@restorationfund.org>
2025-12-10 12:39:13 -0800

    data cleaning part 1 :)

--- scripts/Sophie Script.R
+++ scripts/Sophie Script.R
@@ -2,7 +2,7 @@
 #install packages#
 
 
-
+#####Instruments#####
 
 #####Temperature of all selected sites 2024-2025 (SURFACE)#####
 
@@ -17,7 +17,18 @@
 #####Dissolved Oxygen of all selected sited 2025 (BOTTOM)#####
 
 
+#####Kelp#####
+install.packages("ggplot2")
+library(ggplot2)
+library(readr)
+Kelp <- read_csv("data/Kelp.csv")
 
+#remove everything not needed#
+Kelp_clean<-subset(Kelp, select = -c(survey_id, site_country, latitude, longitude, region, stipes, observer, date_start, date_end))
+View(Kelp_clean)
+
+#add density column#
+Kelp_clean$density<-Kelp_clean$amount/Kelp_clean$distance
 
 #####Kelp densities for all species deep vs. shallow years 2024 and 2025 stacked#####
 ##(how do you stack densities/error bars)##
